e08afced78aa762e83db511f17850e0b9cac1122
Kazushi Nakamura <49474508+kzsh4124@users.noreply.github.com>
2021-12-28 17:31:48 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dockerで構築した授業用Java Project
+
+## 環境
+ WSL2 + Docker Desktop + VSCode(remote container)  
+ 参考 ![Link](https://qiita.com/mfunaki/items/dc09764cb08bb787c261#%E3%83%AD%E3%83%BC%E3%82%AB%E3%83%AB%E3%81%8B%E3%82%89%E3%82%B3%E3%83%B3%E3%83%86%E3%83%8A%E4%B8%8A%E3%81%AB%E9%96%8B%E7%99%BA%E7%92%B0%E5%A2%83%E3%82%92%E7%A7%BB%E3%82%8B)
---
+++ src/Hello-Java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("運命を殺しに来たんだ");
+        System.out.println("この手も体も偽物だなんて今更");
+        System.out.println("物語を聞かせよう");
+        System.out.println("未来を手にするのは");
+        System.out.println("誰?？");
+    }
+}
